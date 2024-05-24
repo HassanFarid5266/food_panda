568f553c642a8824fd441fec5c7473780500ad6a
HassanFarid5266 <hassanbaba8142@gmail.com>
2024-05-24 10:56:19 -0700

    first commit

---
+++ assest/js/index.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.querySelector(".slider");
+  const leftButton = document.querySelector(".nav-button.left");
+  const rightButton = document.querySelector(".nav-button.right");
+
+  function updateButtons() {
+    const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
+    if (slider.scrollLeft > 0) {
+      leftButton.style.display = "block";
+    } else {
+      leftButton.style.display = "none";
+    }
+    if (slider.scrollLeft < maxScrollLeft) {
+      rightButton.style.display = "block";
+    } else {
+      rightButton.style.display = "none";
+    }
+  }
+
+  leftButton.addEventListener("click", function () {
+    slider.scrollBy({
+      left: -300,
+      behavior: "smooth",
+    });
+  });
+
+  rightButton.addEventListener("click", function () {
+    slider.scrollBy({
+      left: 300,
+      behavior: "smooth",
+    });
+  });
+
+  slider.addEventListener("scroll", updateButtons);
+
+  updateButtons();
+});
+
+// color change 
+$('.favme').click(function() {
+  $(this).toggleClass('active');
+});
